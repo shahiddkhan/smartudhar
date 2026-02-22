649759628cc06c5da446d36928631ab915f9ba66
shahiddkhan <shahidkhan176aa@gmail.com>
2026-02-22 23:49:04 +0530

    Fix Turbopack build error

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
-const withPWA = require("next-pwa")({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-});
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  turbopack: {},
+};
 
-module.exports = withPWA({
-  reactStrictMode: true,
-});
+module.exports = nextConfig;
